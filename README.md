0ba3578d2f96dc39c87c506263b89bacf86fa4d7
Josip Miskovic <josipmiskovic@gmail.com>
2024-08-02 22:45:15 +0200

    Adapt to Jolt physics; remove state snapshoting

--- README.md
+++ README.md
@@ -10,8 +10,7 @@ world = lovr.physics.newWorld()
 -- (create some colliders)
 
 function lovr.draw(pass)
-  phywire.draw(pass, world, phywire.render_shapes)  -- render solid geometry
-  -- phywire.draw(pass, world)                      -- render in-depth visualizations
+  phywire.draw(pass, world)             -- render with default visualization options (shapes only)
 end
 ```
 
@@ -22,7 +21,6 @@ Aside from simple rendering of colliders, the library can visualize the physical
 * show angular velocity gizmos around each collider
 * visualize the joints between colliders
 * show information on collision contacts
-* save and restore a snapshot of physics state
 
 ![slideshow](slideshow.gif)
 
@@ -51,64 +49,26 @@ The third argument in `phywire.draw(pass, world, options)` receives a table with
 Any visualization can be disabled by overriding some options:
 
 ```Lua
-phywire.draw(pass, world, {
-  show_shapes = true,           -- draw collider shapes
-  show_velocities = true,       -- vector showing direction and magnitude of collider linear velocity
-  show_angulars = true,         -- gizmo displaying the collider's angular velocity
-  show_joints = true,           -- show joints between colliders
-  show_contacts = true,         -- show collision contacts (quite inefficient, triples the needed collision computations)
-})
+phywire.options.show_shapes = true           -- draw collider shapes (on by default)
+phywire.options.show_velocities = true       -- vector showing direction and magnitude of collider linear velocity
+phywire.options.show_angulars = true         -- gizmo displaying the collider's angular velocity
+phywire.options.show_joints = true           -- show joints between colliders
+phywire.options.show_contacts = true         -- show collision contacts (quite inefficient, triples the needed collision computations)
 ```
 
 The `wireframe` flag is used to render shapes in wireframe mode. The `overdraw` flag disables the depth buffer test. This allows for some useful combinations.
 
-* `{wireframe=false, overdraw=false}` draws solid geometry, provides quick and simple replacement for rendering of "physical" scene
-* `{wireframe=true, overdraw=true}` renders on top of already drawn scene, this allows users to make sure their rendering is aligned with the physics state
-* `{wireframe=true, overdraw=false}` renders wireframe visualizations but respects existing scene geometry (visuals introduce less noise, more usable for VR)
+* `wireframe=false, overdraw=false` draws solid geometry, provides quick and simple replacement for rendering of "physical" scene
+* `wireframe=true, overdraw=true` renders on top of already drawn scene, this allows users to make sure their rendering is aligned with the physics state
+* `wireframe=true, overdraw=false` renders wireframe visualizations but respects existing scene geometry (visuals introduce less noise, more usable for VR)
 
 
 Various other options can be overridden, things like the size of each visualization type, sensitivities, and gizmo colors. Check the `m.options` table for more info.
 
-The `phywire.render_shapes` table holds a preset for drawing the solid geometry of shapes without any other debugging visualizations; it can be given in place of *options* table.
-
 ## Controlling colors
 
-The phywire visualization assigns different colors for each shape. To start off with, these colors are assigned from an internal palette by default, which is a quickest way for user to visualize their colliders. Going on from here there are few more options for having more control over color choices.
+The phywire visualization assigns a color for each shape. By default these colors are chosen from an internal palette, which is a quickest way to visualize colliders in lovr project. Going on from here there are few more options for having more control over color choices.
 
-Second easiest option is to replace the internal palette and bring in a new set of colors. The shape colors will be chosen sequentially from the palette, which makes it hard to pick a color for each individual shape. To supply the custom palette, compose the nested list of colors pass it inside the `options.shapes_palette`. The list should have at least one color for a boring monochromatic look.
+Second easiest option is to replace the internal palette and bring in a new set of colors. The shape colors will be chosen sequentially from the palette, which makes it hard to control the color for each individual shape. To use the custom color palette, compose the nested list of colors and assign the `options.shapes_palette` to that table. The list can contain just one color, which results in monochromatic rendering of all the shapes.
 
-User can also specify individual colors for each shape. Phywire will try to look up `options.shape_colors[shape]` and if found that color will be used. Keys of this table are individual shapes, values are the colors used for that shape in `{r,g,b}` or hexcode format. The color can also be specified inside the shape's attached userdata using `shape:setUserData({color = {r,g,b})`. The userdata color is convenient because it can preserve colors across snapshots (see below).
-
-## Snapshots
-
-The current world state can be stored in a snapshot, and the snapshot can be used to re-create the state.
-
-```Lua
-snapshot = phywire.toSnapshot(world)
--- construct a new world from a saved snapshot
-world = phywire.fromSnapshot(snapshot)
--- or add the snapshot into the already existing world
-phywire.fromSnapshot(state, world)
-```
-
-The snapshot is a nested Lua table containing the complete data necessary to reconstruct the physics state:
-* all the world parameters like gravity and velocity damping
-* all colliders with current poses, velocities, parameters
-* all the shapes in colliders, including their dimensions and parameters
-* all the joints connecting the colliders
-
-Because the snapshot is just a table of Lua primitives, it can easily be serialized and saved to disk.
-
-```Lua
--- save/load using the `serpent` lib (not included)
-lovr.filesystem.write('snapshot01.lua', serpent.block(snapshot))
-snapshot_str = lovr.filesystem.read('snapshot01.lua')
-ok, snapshot = serpent.load(snapshot_str)
-if ok then
-  world = phywire.fromSnapshot(snapshot)
-end
-```
-
-Naturally, any references to colliders/shapes/joints in the physics world will need to be re-assigned after the snapshot is loaded.
-
-**Limitations:** collider tags, mesh shapes and terrain shapes are not supported for snapshots. Tags will be ignored; unsupported shapes will be skipped after printing a warning in the console output.
+User can also specify individual colors for each shape. Phywire will try to look up `options.shape_colors[shape]`. If found, that color will be used. Keys of the `shape_colors` table are individual shapes, values are the colors used for that shape in `{r,g,b}` or hexcode format. For example, `phywire.options.shape_colors[my_shape] = 0xff00ff` will specify manual color for a single element.
--- phywire.lua
+++ phywire.lua
@@ -1,19 +1,21 @@
 local m = {}
 
+-- default options table
 m.options = {
   -- drawing options
-  wireframe = true,             -- show shapes in wireframe instead of filled geometry
-  overdraw = true,              -- force elements to render over existing scene (ignore depth buffer check)
-  show_shapes = true,           -- draw collider shapes (mesh and terrain not supported!)
-  show_velocities = true,       -- vector showing direction and magnitude of collider linear velocity
-  show_angulars = true,         -- gizmo displaying the collider's angular velocity
-  show_joints = true,           -- show joints between colliders
-  show_contacts = true,         -- show collision contacts (quite inefficient, triples the needed collision computations)
-  geometry_segments = 15,       -- complexity of rendered geometry (number of segments in spheres, circles, cylinders, cones)
+  wireframe = false,             -- show shapes in wireframe instead of filled geometry
+  overdraw = false,              -- force elements to render over existing scene (ignore depth buffer check)
+  show_shapes = true,            -- draw collider shapes (mesh and terrain not supported!)
+  show_aabb = false,             -- draw each shape's AABB box
+  show_velocities = false,       -- vector showing direction and magnitude of collider linear velocity
+  show_angulars = false,         -- gizmo displaying the collider's angular velocity
+  show_joints = false,           -- show joints between colliders
+  show_contacts = false,         -- show collision contacts (quite inefficient, triples the needed collision computations)
+  geometry_segments = 22,        -- complexity of rendered geometry (number of segments in spheres, circles, cylinders, cones)
   -- sizes of visualized elements
-  velocity_sensitivity = 0.1,   -- velocity multiplier to scale the displayed velocity vectors
+  velocity_sensitivity = 0.1,    -- velocity multiplier to scale the displayed velocity vectors
   velocity_arrow_size = 0.002,
-  angular_sensitivity = 4,      -- angular velocity multiplier for scaling the gizmo angles
+  angular_sensitivity = 4,       -- angular velocity multiplier for scaling the gizmo angles
   angular_gizmo_size = 0.05,
   joint_label_size = 0.05,
   joint_anchor_size = 0.01,
@@ -48,71 +50,96 @@ m.options = {
     {0.600, 0.498, 0.451},
   }
 }
-m.options.__index = m.options
-
-m.render_shapes = { -- options preset for drawing filled shapes only
-  wireframe = false,
-  overdraw = false,
-  show_shapes = true,
-  show_velocities = false,
-  show_angulars = false,
-  show_joints = false,
-  show_contacts = false,
-  geometry_segments = 64,
-}
 
 m.next_color_index = 1 -- index of last chosen palette color
 m.shown_warning = false
 
-function m.drawCollider(pass, collider, options)
+local aabb_points = {}
+for i=1,8 do
+  aabb_points[i] = lovr.math.newVec3()
+end
+
+function m.setColor(shape_or_collider, color)
+  if shape_or_collider.getType then
+    local shape = shape_or_collider
+    m.options.shape_colors[shape] = color
+  elseif shape_or_collider.getShapes then
+    local collider = shape_or_collider
+    for i, shape in ipairs(collider:getShapes()) do
+      m.options.shape_colors[shape] = color
+    end
+  end
+end
+
+
+function m.drawCollider(pass, collider, clip_from_world)
+  local options = m.options
   for _, shape in ipairs(collider:getShapes()) do
-    if not options.shape_colors[shape] then
-      local userdata = shape:getUserData()
-      if type(userdata) == 'table' and userdata.color then
-        options.shape_colors[shape] = userdata.color
-      elseif tostring(userdata) == 'Model' then
-        options.shape_colors[shape] = {1,1,1}
-      else
+    local skip = false
+    if not skip then
+      if not options.shape_colors[shape] then
         options.shape_colors[shape] = options.shapes_palette[m.next_color_index]
         m.next_color_index = 1 + (m.next_color_index % #options.shapes_palette)
       end
-    end
-    pass:setColor(options.shape_colors[shape])
-    local pose = mat4(collider:getPose()):translate(shape:getPosition()):rotate(shape:getOrientation())
-    local shape_type = shape:getType()
-    if shape_type == 'box' then
-      pass:box(pose:scale(shape:getDimensions()))
-    elseif shape_type == 'sphere' then
-      pass:sphere(pose:scale(shape:getRadius()), options.geometry_segments, options.geometry_segments)
-    elseif shape_type == 'cylinder' then
-      local l, r = shape:getLength(), shape:getRadius()
-      pass:cylinder(pose:scale(r, r, l), true, 0, 2 * math.pi, options.geometry_segments)
-    elseif shape_type == 'capsule' then
-      local l, r = shape:getLength(), shape:getRadius()
-      pass:capsule(pose:scale(r, r, l), options.geometry_segments)
-    else
-      local userdata = collider:getUserData()
-      if userdata and userdata.draw then
-        userdata:draw(pass, pose)
-      elseif tostring(userdata) == 'Model' then
-        pass:draw(userdata, pose)
-      elseif not m.shown_warning then -- not supported
-        print('Warning: TerrainShape and MeshShape are not supported and will not be rendered')
-        m.shown_warning = true
+      pass:setColor(options.shape_colors[shape])
+      local pose = mat4(collider:getPose()):mul(mat4(shape:getOffset()))
+      local shape_type = shape:getType()
+      if shape_type == 'box' then
+        pass:box(pose:scale(shape:getDimensions()))
+      elseif shape_type == 'sphere' then
+        pass:sphere(pose:scale(shape:getRadius()), options.geometry_segments, options.geometry_segments)
+      elseif shape_type == 'cylinder' then
+        local l, r = shape:getLength(), shape:getRadius()
+        pose
+          :scale(r, r, l)
+        pass:cylinder(pose, true, 0, 2 * math.pi, options.geometry_segments)
+      elseif shape_type == 'capsule' then
+        local l, r = shape:getLength(), shape:getRadius()
+        pose
+          :rotate(math.pi / 2, -1, 0, 0) -- jolt has them oriented differently
+          :scale(r, r, l)
+        pass:capsule(pose, options.geometry_segments)
+      else
+        if not m.shown_warning then -- not supported
+          print('Warning: TerrainShape and MeshShape are not supported and will not be rendered')
+          m.shown_warning = true
+        end
       end
+      m.drawn_shapes = m.drawn_shapes + 1
     end
   end
 end
 
 
-function m.drawShapes(pass, world, options)
+function m.drawShapes(pass, world)
+  m.drawn_shapes = 0
   for _, collider in ipairs(world:getColliders()) do
-    m.drawCollider(pass, collider, options)
+    m.drawCollider(pass, collider, clip_from_world)
   end
 end
 
 
-function m.drawJoints(pass, world, options)
+function m.drawAABBs(pass, world)
+  pass:setColor(1, 1, 1, 0.2)
+  for _, collider in ipairs(world:getColliders()) do
+    for _, shape in ipairs(collider:getShapes()) do
+      local minx, maxx, miny, maxy, minz, maxz = shape:getAABB()
+      pass:box(
+        (minx + maxx) / 2, -- x
+        (miny + maxy) / 2, -- y
+        (minz + maxz) / 2, -- z
+        maxx - minx, -- w
+        maxy - miny, -- h
+        maxz - minz, -- d
+        0, 0, 0, 0,
+        'line')
+    end
+  end
+end
+
+
+function m.drawJoints(pass, world)
+  local options = m.options
   pass:setColor(1,1,1)
   for i, collider in ipairs(world:getColliders()) do
     for j, joint in ipairs(collider:getJoints()) do
@@ -125,8 +152,8 @@ function m.drawJoints(pass, world, options)
           pass:sphere(vec3(x1, y1, z1), options.joint_anchor_size, options.geometry_segments)
           pass:sphere(vec3(x2, y2, z2), options.joint_anchor_size, options.geometry_segments)
           pass:setColor(options.joint_axis_color)
-          pass:line(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.05),
-                    vec3(x1, y1, z1):lerp(x2, y2, z2, 0.95))
+          --pass:line(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.05),
+          --          vec3(x1, y1, z1):lerp(x2, y2, z2, 0.95))
           pass:setColor(options.joint_label_color)
           local pose = mat4():target(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.5), vec3(x2, y2, z2)):rotate(-math.pi/2, 0,1,0)
           pass:text(joint_type, pose:scale(options.joint_label_size))
@@ -150,20 +177,35 @@ function m.drawJoints(pass, world, options)
           pass:line(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.05),
                     vec3(x1, y1, z1):lerp(x2, y2, z2, 0.95))
           pass:setColor(options.joint_label_color)
-          local pose = mat4():target(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.5), vec3(x2, y2, z2)):rotate(-math.pi/2, 0,1,0)
+          local pose = mat4()
+          pose:target(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.5), vec3(x2, y2, z2))
+          pose:rotate(-math.pi/2, 0,1,0)
           pass:text(joint_type, pose:scale(options.joint_label_size))
+
         elseif joint_type == 'hinge' then
-          local x1, y1, z1,  x2, y2, z2 = joint:getAnchors() -- anchors are colocated when joint is satisfied
+          local x1, y1, z1,  x2, y2, z2 = joint:getAnchors()
           local ax, ay, az = joint:getAxis()
           local angle = joint:getAngle()
           pass:setColor(options.joint_anchor_color)
           pass:sphere(vec3(x1, y1, z1), options.joint_anchor_size, options.geometry_segments)
           pass:sphere(vec3(x2, y2, z2), options.joint_anchor_size, options.geometry_segments)
           pass:setColor(options.joint_axis_color)
+          local end_axis = vec3(ax, ay, az):mul(options.joint_line_size):add(x1, y1, z1)
           pass:line(vec3(x1, y1, z1),  -- line from anchor down the axis
-                    vec3(ax, ay, az):mul(options.joint_line_size):add(x1, y1, z1))
+                    end_axis)
           pass:setColor(options.joint_label_color)
-          local pose = mat4(x1, y1, z1,  -angle, ax, ay, az)
+          local pose = mat4(end_axis,  -angle, ax, ay, az)
+          pass:text(joint_type, pose:scale(options.joint_label_size))
+        elseif joint_type == 'weld' then
+          local x1, y1, z1,  x2, y2, z2 = joint:getAnchors()
+          pass:setColor(options.joint_anchor_color)
+          pass:sphere(vec3(x1, y1, z1), options.joint_anchor_size, options.geometry_segments)
+          pass:sphere(vec3(x2, y2, z2), options.joint_anchor_size, options.geometry_segments)
+          pass:setColor(options.joint_axis_color)
+          pass:line(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.05),
+                    vec3(x1, y1, z1):lerp(x2, y2, z2, 0.95))
+          pass:setColor(options.joint_label_color)
+          local pose = mat4(vec3(x1, y1, z1):lerp(x2, y2, z2, 0.5), colliderA:getOrientation())
           pass:text(joint_type, pose:scale(options.joint_label_size))
         end
       end
@@ -172,7 +214,8 @@ function m.drawJoints(pass, world, options)
 end
 
 
-function m.drawVelocities(pass, world, options)
+function m.drawVelocities(pass, world)
+  local options = m.options
   for i, collider in ipairs(world:getColliders()) do
     local pos = vec3(collider:getPosition())
     local vel = vec3(collider:getLinearVelocity())
@@ -188,7 +231,8 @@ function m.drawVelocities(pass, world, options)
 end
 
 
-function m.drawAngulars(pass, world, options)
+function m.drawAngulars(pass, world)
+  local options = m.options
   local pose = mat4()
   for i, collider in ipairs(world:getColliders()) do
     local ang = vec3(collider:getAngularVelocity()):mul(options.angular_sensitivity)
@@ -229,7 +273,8 @@ function m.drawAngulars(pass, world, options)
 end
 
 
-function m.drawCollisions(pass, world, options)
+function m.drawCollisions(pass, world)
+  local options = m.options
   local tmat = mat4()
   local tvec = vec3()
   world:update(0,
@@ -257,9 +302,10 @@ function m.drawCollisions(pass, world, options)
 end
 
 
-function m.draw(pass, world, options)
-  options = setmetatable(options or {}, m.options)
+function m.draw(pass, world)
+  local options = m.options
   pass:push('state')
+  pass:setShader()
   if options.wireframe then
     pass:setWireframe(true)
     if options.overdraw then
@@ -267,10 +313,11 @@ function m.draw(pass, world, options)
     end
   end
   if options.show_shapes then     m.drawShapes(pass, world, options)     end
-  pass:setWireframe(false) -- wireframe option only affects shapes
+  if options.show_aabb then       m.drawAABBs(pass, world, options)      end
   if options.overdraw then
     pass:setDepthTest()
   end
+  pass:setShader() -- drawing text and other gizmos with default shader
   if options.show_joints then     m.drawJoints(pass, world, options)     end
   if options.show_velocities then m.drawVelocities(pass, world, options) end
   if options.show_angulars then   m.drawAngulars(pass, world, options)   end
@@ -278,173 +325,5 @@ function m.draw(pass, world, options)
   pass:pop('state')
 end
 
-
-function m.toSnapshot(world)
-  local snapshot = {}
-  snapshot.colliders = {}
-  snapshot.joints = {}
-  snapshot.world = {}
-  snapshot.world.sleepingallowed = world:isSleepingAllowed()
-  snapshot.world.angulardamping = world:getAngularDamping()
-  snapshot.world.lineardamping = world:getLinearDamping()
-  snapshot.world.responsetime = world:getResponseTime()
-  snapshot.world.tightness = world:getTightness()
-  snapshot.world.stepcount = world:getStepCount()
-  snapshot.world.gravity = {world:getGravity()}
-  local joints_set = {}
-  local collider_to_index = {}
-  for i, collider in ipairs(world:getColliders()) do
-    local c = {}
-    c.pose = {collider:getPose()}
-    c.angularvelocity = {collider:getAngularVelocity()}
-    c.linearvelocity = {collider:getLinearVelocity()}
-    c.angulardamping = collider:getAngularDamping()
-    c.lineardamping = collider:getLinearDamping()
-    c.restitution = collider:getRestitution()
-    c.friction = collider:getFriction()
-    c.mass = collider:getMass()
-    -- c.massdata = {collider:getMassData()} -- unsupported
-    -- c.tag = collider:getTag() -- unsupported; cannot query World for possible tags
-    c.sleepingallowed = collider:isSleepingAllowed()
-    c.gravityignored = collider:isGravityIgnored()
-    c.kinematic = collider:isKinematic()
-    c.userdata = collider:getUserData()
-    c.awake = collider:isAwake()
-    c.shapes = {}
-    for _, shape in ipairs(collider:getShapes()) do
-      local s = {}
-      s.orientation = {shape:getOrientation()}
-      s.position = {shape:getPosition()}
-      s.userdata = shape:getUserData()
-      s.enabled = shape:isEnabled()
-      s.sensor = shape:isSensor()
-      s.type = shape:getType()
-      if s.type == 'box' then
-        s.dimensions = {shape:getDimensions()}
-        table.insert(c.shapes, s)
-      elseif s.type == 'sphere' then
-        s.radius = shape:getRadius()
-        table.insert(c.shapes, s)
-      elseif s.type == 'capsule' or s.type == 'cylinder' then
-        s.radius = shape:getRadius()
-        s.length = shape:getLength()
-        table.insert(c.shapes, s)
-      elseif not m.shown_warning then -- not supported
-        print('Warning: TerrainShape and MeshShape are not supported in snapshot')
-        m.shown_warning = true
-      end
-    end
-    table.insert(snapshot.colliders, c)
-    for _, joint in ipairs(collider:getJoints()) do
-      joints_set[joint] = true
-    end
-    collider_to_index[collider] = i
-  end
-  for joint, _ in pairs(joints_set) do
-    local j = {}
-    local colliderA, colliderB = joint:getColliders()
-    j.indexa = collider_to_index[colliderA]
-    j.indexb = collider_to_index[colliderB]
-    j.userdata = joint:getUserData()
-    j.enabled = joint:isEnabled()
-    j.type = joint:getType()
-    if j.type == 'ball' then
-      j.responsetime = joint:getResponseTime()
-      j.tightness = joint:getTightness()
-      j.anchors = {joint:getAnchors()}
-    elseif j.type == 'distance' then
-      j.anchors = {joint:getAnchors()}
-      j.distance = joint:getDistance()
-      j.responsetime = joint:getResponseTime()
-      j.tightness = joint:getTightness()
-    elseif j.type == 'hinge' then
-      j.anchors = {joint:getAnchors()}
-      j.axis = {joint:getAxis()}
-      j.limits = {joint:getLimits()}
-    elseif j.type == 'slider' then
-      j.axis = {joint:getAxis()}
-      j.limits = {joint:getLimits()}
-    end
-    table.insert(snapshot.joints, j)
-  end
-  return snapshot
-end
-
-
-function m.fromSnapshot(snapshot, world)
-  world = world or lovr.physics.newWorld()
-  world:setSleepingAllowed(snapshot.world.sleepingallowed)
-  world:setAngularDamping(snapshot.world.angulardamping)
-  world:setLinearDamping(snapshot.world.lineardamping)
-  world:setResponseTime(snapshot.world.responsetime)
-  world:setTightness(snapshot.world.tightness)
-  world:setStepCount(snapshot.world.stepcount)
-  world:setGravity(unpack(snapshot.world.gravity))
-  local index_to_collider = {}
-  for _, c in ipairs(snapshot.colliders) do
-    collider = world:newCollider()
-    collider:setPose(unpack(c.pose))
-    collider:setAngularVelocity(unpack(c.angularvelocity))
-    collider:setLinearVelocity(unpack(c.linearvelocity))
-    collider:setAngularDamping(c.angulardamping)
-    collider:setLinearDamping(c.lineardamping)
-    collider:setRestitution(c.restitution)
-    collider:setFriction(c.friction)
-    collider:setMass(c.mass)
-    collider:setSleepingAllowed(c.sleepingallowed)
-    collider:setGravityIgnored(c.gravityignored)
-    collider:setKinematic(c.kinematic)
-    collider:setUserData(c.userdata)
-    collider:setAwake(c.awake)
-    for _, s in ipairs(c.shapes) do
-      local shape
-      if s.type == 'box' then
-        shape = lovr.physics.newBoxShape(unpack(s.dimensions))
-      elseif s.type == 'sphere' then
-        shape = lovr.physics.newSphereShape(s.radius)
-      elseif s.type == 'capsule' then
-        shape = lovr.physics.newCapsuleShape(s.radius, s.length)
-      elseif s.type == 'cylinder' then
-        shape = lovr.physics.newCylinderShape(s.radius, s.length)
-      end
-      collider:addShape(shape)
-      shape:setPosition(unpack(s.position))
-      shape:setOrientation(unpack(s.orientation))
-      shape:setUserData(s.userdata)
-      shape:setEnabled(s.enabled)
-      shape:setSensor(s.sensor)
-    end
-    table.insert(index_to_collider, collider)
-  end
-  for _, j in pairs(snapshot.joints) do
-    local joint
-    local colliderA = index_to_collider[j.indexa]
-    local colliderB = index_to_collider[j.indexb]
-    if j.type == 'ball' then
-      joint = lovr.physics.newBallJoint(colliderA, colliderB)
-      joint:setAnchor(unpack(j.anchors))
-      joint:setResponseTime(j.responsetime)
-      joint:setTightness(j.tightness)
-    elseif j.type == 'distance' then
-      joint = lovr.physics.newDistanceJoint(colliderA, colliderB)
-      joint:setAnchors(unpack(j.anchors))
-      joint:setDistance(j.distance)
-      joint:setResponseTime(j.responsetime)
-      joint:setTightness(j.tightness)
-    elseif j.type == 'hinge' then
-      joint = lovr.physics.newHingeJoint(colliderA, colliderB)
-      joint:setAnchor(unpack(j.anchors))
-      joint:setAxis(unpack(j.axis))
-      joint:setLimits(unpack(j.limits))
-    elseif j.type == 'slider' then
-      joint = lovr.physics.newSliderJoint(colliderA, colliderB)
-      joint:setAxis(unpack(j.axis))
-      joint:setLimits(unpack(j.limits))
-    end
-    joint:setUserData(j.userdata)
-    joint:setEnabled(j.enabled)
-  end
-  return world
-end
 
 return m
